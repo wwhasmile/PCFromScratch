9e539cb1b7f8bd5b0751c28d797c38b7c37597e4
wwhasmile <uwuhasmile@proton.me>
2026-05-19 12:11:41 +0300

    CPU socket cleanup

--- PCFromScratch.DBModels/CpuSocket.cs
+++ PCFromScratch.DBModels/CpuSocket.cs
@@ -3,19 +3,5 @@
 public class CpuSocket
 {
     public Guid Id { get; set; }
-    public string Name { get; set; } = string.Empty;
-
-    public CpuSocket(string name) : this(Guid.NewGuid(), name)
-    {
-    }
-
-    public CpuSocket(Guid id, string name)
-    {
-        Id = id;
-        Name = name;
-    }
-
-    private CpuSocket()
-    {
-    }
+    public required string Name { get; set; }
 }
