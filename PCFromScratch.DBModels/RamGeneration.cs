081e9ebf66b32551e0d05bffca4504947031c8a8
wwhasmile <uwuhasmile@proton.me>
2026-05-19 12:12:06 +0300

    RAM generation cleanup

--- PCFromScratch.DBModels/RamGeneration.cs
+++ PCFromScratch.DBModels/RamGeneration.cs
@@ -3,19 +3,5 @@ namespace PCFromScratch.DBModels;
 public class RamGeneration
 {
     public Guid Id { get; set; }
-    public string Name { get; set; } = string.Empty;
-
-    public RamGeneration(string name) : this(Guid.CreateVersion7(), name)
-    {
-    }
-
-    public RamGeneration(Guid id, string name)
-    {
-        Id = id;
-        Name = name;
-    }
-
-    private RamGeneration()
-    {
-    }
+    public required string Name { get; set; }
 }
