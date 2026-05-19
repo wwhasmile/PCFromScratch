86ef4ea46d2097af32c9dfa5c8a837b4426b2ddb
wwhasmile <uwuhasmile@proton.me>
2026-05-19 12:11:48 +0300

    Manufacturer cleanup

--- PCFromScratch.DBModels/Manufacturer.cs
+++ PCFromScratch.DBModels/Manufacturer.cs
@@ -3,19 +3,5 @@ namespace PCFromScratch.DBModels;
 public class Manufacturer
 {
     public Guid Id { get; set; }
-    public string Name { get; set; } = string.Empty;
-
-    public Manufacturer(string name) : this(Guid.CreateVersion7(), name)
-    {
-    }
-
-    public Manufacturer(Guid id, string name)
-    {
-        Id = id;
-        Name = name;
-    }
-
-    private Manufacturer()
-    {
-    }
+    public required string Name { get; set; }
 }
