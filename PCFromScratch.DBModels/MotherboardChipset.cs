9892735775c625792807bc0b38e56fcdfe5ee6dd
wwhasmile <uwuhasmile@proton.me>
2026-05-19 12:11:54 +0300

    Motherboard chipset cleanup

--- PCFromScratch.DBModels/MotherboardChipset.cs
+++ PCFromScratch.DBModels/MotherboardChipset.cs
@@ -3,19 +3,5 @@ namespace PCFromScratch.DBModels;
 public class MotherboardChipset
 {
     public Guid Id { get; set; }
-    public string Name { get; set; } = string.Empty;
-
-    public MotherboardChipset(string name) : this(Guid.CreateVersion7(), name)
-    {
-    }
-
-    public MotherboardChipset(Guid id, string name)
-    {
-        Id = id;
-        Name = name;
-    }
-
-    private MotherboardChipset()
-    {
-    }
+    public required string Name { get; set; }
 }
