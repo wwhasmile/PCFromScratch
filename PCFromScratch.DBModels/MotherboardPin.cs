3aa520f418c96d73d1bec2cadead038b2116d63b
wwhasmile <uwuhasmile@proton.me>
2026-05-19 12:12:00 +0300

    Motherboard pin cleanup

--- PCFromScratch.DBModels/MotherboardPin.cs
+++ PCFromScratch.DBModels/MotherboardPin.cs
@@ -3,19 +3,5 @@ namespace PCFromScratch.DBModels;
 public class MotherboardPin
 {
     public Guid Id { get; set; }
-    public string Name { get; set; } = string.Empty;
-
-    public MotherboardPin(string name) : this(Guid.CreateVersion7(), name)
-    {
-    }
-
-    public MotherboardPin(Guid id, string name)
-    {
-        Id = id;
-        Name = name;
-    }
-
-    private MotherboardPin()
-    {
-    }
+    public required string Name { get; set; }
 }
