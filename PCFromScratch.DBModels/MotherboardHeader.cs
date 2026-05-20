2dd37d7baa8e68eac6d3090f4ece1a9ec710f8e6
wwhasmile <uwuhasmile@proton.me>
2026-05-20 11:43:26 +0300

    Motherboard header improvement

--- PCFromScratch.DBModels/MotherboardHeader.cs
+++ PCFromScratch.DBModels/MotherboardHeader.cs
@@ -5,8 +5,8 @@ public class MotherboardHeader
     public required Guid MotherboardId { get; set; }
     public required Guid PinId { get; set; }
 
-    public required Motherboard? Motherboard;
-    public required MotherboardPin? Pin;
+    public required Motherboard Motherboard { get; set; }
+    public required MotherboardPin Pin { get; set; }
 
     public int Count { get; set; }
 }
