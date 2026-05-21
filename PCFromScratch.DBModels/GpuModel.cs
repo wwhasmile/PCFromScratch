5061bd823c48390bb0fe83bf67eae92ca82dda46
wwhasmile <uwuhasmile@proton.me>
2026-05-21 12:12:05 +0300

    GPU model DB model

---
+++ PCFromScratch.DBModels/GpuModel.cs
@@ -0,0 +1,9 @@
+using PCFromScratch.Common;
+
+namespace PCFromScratch.DBModels;
+
+public class GpuModel
+{
+    public required Guid Id { get; set; }
+    public required string Name { get; set; }
+}
