7392d51bc780f390c3faed982cf732bad7da7e00
wwhasmile <uwuhasmile@proton.me>
2026-05-27 18:54:15 +0300

    ICpuRepository

---
+++ PCFromScratch.Repository/ICpuRepository.cs
@@ -0,0 +1,14 @@
+﻿using PCFromScratch.DBModels;
+
+namespace PCFromScratch.Repository;
+
+public interface ICpuRepository
+{
+    IAsyncEnumerable<Cpu> GetCpus();
+
+    Task<Cpu?> GetCpu(Guid id);
+
+    Task AddCpu(Cpu cpu);
+    Task UpdateCpu(Cpu cpu);
+    Task RemoveCpu(Guid id);
+}
