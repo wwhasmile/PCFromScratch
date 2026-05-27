c0ccdd840abcdb910f5e9d29a76a73a55c7402ef
wwhasmile <uwuhasmile@proton.me>
2026-05-27 18:54:22 +0300

    IGpuRepository

---
+++ PCFromScratch.Repository/IGpuRepository.cs
@@ -0,0 +1,14 @@
+using PCFromScratch.DBModels;
+
+namespace PCFromScratch.Repository;
+
+public interface IGpuRepository
+{
+    IAsyncEnumerable<Gpu> GetGpus();
+
+    Task<Gpu?> GetGpu();
+
+    Task AddGpu(Gpu gpu);
+    Task UpdateGpu(Gpu gpu);
+    Task RemoveGpu(Guid id);
+}
