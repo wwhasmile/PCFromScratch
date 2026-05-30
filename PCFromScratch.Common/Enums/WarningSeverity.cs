191012d29ad25154dfc2b27d4425bc87d0fde66f
SkyWarpX27X <mykhajloost@gmail.com>
2026-05-30 13:48:17 +0300

    Warnings display

---
+++ PCFromScratch.Common/Enums/WarningSeverity.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PCFromScratch.Common;
+
+public enum WarningSeverity
+{
+    [Display(Name="Несумісність")]
+    Incompatibility,
+    [Display(Name = "Вузьке місце")]
+    Bottleneck,
+    [Display(Name = "Інфо")]
+    Info //Something that is not critical, but better to mention it
+}
---
+++ PCFromScratch.Common/Warning.cs
@@ -0,0 +1,17 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace PCFromScratch.Common;
+public partial class Warning : ObservableObject
+{
+    [ObservableProperty]
+    private EnumWithName<WarningSeverity> _severity;
+    [ObservableProperty]
+    private string _message;
+
+    public Warning(WarningSeverity severity, string message)
+    {
+        _severity = severity.GetEnumWithName();
+        _message = message;
+    }
+    
+}
