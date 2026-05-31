0769dddf4e4b65df8e3b4f9abc12bf86638d3d40
wwhasmile <uwuhasmile@proton.me>
2026-05-31 18:37:35 +0300

    Changing order in storage cooler repository

--- PCFromScratch.Repository/StorageCoolerRepository.cs
+++ PCFromScratch.Repository/StorageCoolerRepository.cs
@@ -9,9 +9,9 @@ public class StorageCoolerRepository(IStorageContext storageContext) : ICoolerRe
 
     public IAsyncEnumerable<Cooler> GetCoolers(string? socket = null) => _storageContext.GetCoolers(socket);
 
-    public Task AddCooler(Cooler cooler) => _storageContext.AddCooler(cooler);
+     public Task<Cooler?> GetCooler(Guid id) => _storageContext.GetCooler(id);
 
-    public Task<Cooler?> GetCooler(Guid id) => _storageContext.GetCooler(id);
+    public Task AddCooler(Cooler cooler) => _storageContext.AddCooler(cooler);
 
     public Task UpdateCooler(Cooler cooler) => _storageContext.UpdateCooler(cooler);
 
