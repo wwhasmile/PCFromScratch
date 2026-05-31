7757cfacdea77f74718a6ec6b41a23658b832b00
wwhasmile <uwuhasmile@proton.me>
2026-05-31 18:37:18 +0300

    Storage motherboard repository

---
+++ PCFromScratch.Repository/StorageMotherboardRepository.cs
@@ -0,0 +1,20 @@
+using PCFromScratch.DBModels;
+using PCFromScratch.Storage;
+
+namespace PCFromScratch.Repository;
+
+public class StorageMotherboardRepository(IStorageContext storageContext) : IMotherboardRepository
+{
+    private readonly IStorageContext _storageContext = storageContext;
+
+    public IAsyncEnumerable<Motherboard> GetMotherboards(string? socket = null)
+    => _storageContext.GetMotherboards(socket);
+
+    public Task<Motherboard?> GetMotherboard(Guid id) => _storageContext.GetMotherboard(id);
+
+    public Task AddMotherboard(Motherboard motherboard) => _storageContext.AddMotherboard(motherboard);
+
+    public Task UpdateMotherboard(Motherboard motherboard) => _storageContext.UpdateMotherboard(motherboard);
+
+    public Task RemoveMotherboard(Guid id) => _storageContext.RemoveMotherboard(id);
+}
