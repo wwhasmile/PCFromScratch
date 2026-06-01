cd8801cc2d31e7789d7615e13f6524461b593c87
SkyWarpX27X <mykhajloost@gmail.com>
2026-06-01 18:41:43 +0300

    Mock cpu/gpu data until db is ready

---
+++ PCFromScratch.Repository/FakeCpu.cs
@@ -0,0 +1,42 @@
+﻿using PCFromScratch.Common;
+using PCFromScratch.DBModels;
+
+namespace PCFromScratch.Repository;
+
+public class FakeCpu : ICpuRepository
+{
+    public async IAsyncEnumerable<Cpu> GetCpus()
+    {
+        var cpus = new List<Cpu>
+        {
+            new Cpu {Id = Guid.NewGuid(), Name = "Intel Core Ultra 7 270K Plus", Socket = "1151", MaxRam = 128, RamGen = "DDR5", Tdp = 165, Packing = CpuPacking.Box },
+            new Cpu {Id = Guid.NewGuid(), Name = "AMD Ryzen 5 Matisse 3600", Socket = "AM5", MaxRam = 256, RamGen = "DDR5", Tdp = 165, Packing = CpuPacking.OEM },
+            new Cpu {Id = Guid.NewGuid(), Name = "Intel Core i9 Raptor Lake Refresh i9-14900K", Socket = "1151", MaxRam = 256, RamGen = "DDR5", Tdp = 165, Packing = CpuPacking.OEM },
+            new Cpu {Id = Guid.NewGuid(), Name = "AMD Ryzen 5 Summit Ridge 1600 OEM 1", Socket = "AM4", MaxRam = 128, RamGen = "DDR5", Tdp = 165, Packing = CpuPacking.Box }
+        };
+        foreach (var cpu in cpus)
+        {
+            yield return cpu;
+        }
+    }
+
+    public Task<Cpu?> GetCpu(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task AddCpu(Cpu cpu)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateCpu(Cpu cpu)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task RemoveCpu(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ PCFromScratch.Repository/FakeGpu.cs
@@ -0,0 +1,42 @@
+﻿using PCFromScratch.DBModels;
+
+namespace PCFromScratch.Repository;
+
+public class FakeGpu : IGpuRepository
+{
+    public async IAsyncEnumerable<Gpu> GetGpus()
+    {
+        var gpus = new List<Gpu>
+        {
+            new Gpu { Id = Guid.NewGuid(), Name = "Gigabyte Radeon RX 9060 XT GAMING OC 16G", Tdp = 450 },
+            new Gpu { Id = Guid.NewGuid(), Name = "Gigabyte GeForce RTX 5080 GAMING OC 16G", Tdp = 320 },
+            new Gpu { Id = Guid.NewGuid(), Name = "MSI GeForce RTX 5070 12G GAMING TRIO OC", Tdp = 355 },
+            new Gpu { Id = Guid.NewGuid(), Name = "Sapphire Radeon RX 9060 XT PULSE 16GB", Tdp = 170 }
+        };
+
+        foreach (var gpu in gpus)
+        {
+            yield return gpu;
+        }
+    }
+
+    public Task<Gpu?> GetGpu(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task AddGpu(Gpu gpu)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateGpu(Gpu gpu)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task RemoveGpu(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+}
