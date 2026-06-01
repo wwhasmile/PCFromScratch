11fafbe14e101fb994db354e066f0d717779ae21
wwhasmile <uwuhasmile@proton.me>
2026-06-01 17:48:43 +0300

    Making every Guid nullable in Pc Dto model

--- PCFromScratch.DTOModels/PcDtoModel.cs
+++ PCFromScratch.DTOModels/PcDtoModel.cs
@@ -2,11 +2,11 @@
 
 public class PcDtoModel
 {
-    public Guid Cpu { get; set; }
-    public Guid Motherboard { get; set; }
-    public Guid Gpu { get; set; }
-    public Guid Ram { get; set; }
-    public Guid Cooler { get; set; }
+    public Guid? Cpu { get; set; }
+    public Guid? Motherboard { get; set; }
+    public Guid? Gpu { get; set; }
+    public Guid? Ram { get; set; }
+    public Guid? Cooler { get; set; }
     public HashSet<Guid> InternalDrives { get; set; } = [];
-    public Guid Psu { get; set; }
+    public Guid? Psu { get; set; }
 }
