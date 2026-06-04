20af7fedbe385ccfb21c2aa4630070cebb24c3de
SkyWarpX27X <mykhajloost@gmail.com>
2026-06-04 14:51:21 +0300

    Update cpu scraper

--- PCFromScratch.DBModels/Cpu.cs
+++ PCFromScratch.DBModels/Cpu.cs
@@ -6,11 +6,14 @@ public class Cpu
 {
     public Guid Id { get; set; }
     public required string Name { get; set; }
+    public required string Link { get; set; }
 
     public required string Socket { get; set; }
     public int Tdp { get; set; }
-    public int MaxRam { get; set; }
     public required string RamGen { get; set; }
     public int RamFrequency { get; set; }
     public CpuPacking Packing { get; set; }
+    public byte[]? Image { get; set; }
+    public string PriceRange { get; set; }
+    public List<Offer> Offers { get; set; } = new ();
 }
--- PCFromScratch.Scrapers/CpuScraper.cs
+++ PCFromScratch.Scrapers/CpuScraper.cs
@@ -1,9 +1,10 @@
 ﻿using System.Globalization;
 using AngleSharp;
+using AngleSharp.Dom;
 using CsvHelper;
 using Microsoft.Playwright;
-
-using PCFromScratch.Scrapers.CSVModels;
+using PCFromScratch.DBModels;
+using PCFromScratch.Common;
 
 namespace PCFromScratch.Scrapers;
 
@@ -11,28 +12,21 @@ namespace PCFromScratch.Scrapers;
 public class CpuScraper
 {
     private static readonly string FilePath = "data/cpus.csv";
-    public static async Task GetCpusForSale()
+    public static async Task GetCpus()
     {
-        if (!File.Exists(FilePath))
-        {
-            string? directoryName = Path.GetDirectoryName(FilePath);
-            if (directoryName is not null && !Directory.Exists(Path.GetDirectoryName(FilePath)))
-            {
-                Directory.CreateDirectory(directoryName);
-            }
-            File.Create(FilePath).Close();
-        }
+        BaseScraper.CreatePath(FilePath);
+
         using var playwright = await Playwright.CreateAsync();
         await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
         {
-            Headless = true // Set to false to see the browser UI
+            Headless = false
         });
         var page = await browser.NewPageAsync();
 
         var cpus = new List<Cpu>();
-        var random = new Random();
 
         var pageLink = "https://ek.ua/ua/list/186/";
+        
         try
         {
             while (pageLink != null)
@@ -40,83 +34,77 @@ public class CpuScraper
                 Console.WriteLine($"Retrieving page: {pageLink}");
                 await page.GotoAsync(pageLink);
 
-                // Simulate Human Behavior
-                await page.EvaluateAsync("window.scrollTo(0, document.body.scrollHeight / 2);");
-                await Task.Delay(random.Next(1000, 2500));
-                await page.EvaluateAsync("window.scrollTo(0, document.body.scrollHeight);");
-                await Task.Delay(random.Next(4500, 8500));
+                await page.EvaluateAsync(@"
+                    let cards = document.querySelectorAll('table.model-short-block');
+                    for (let i = 0; i < cards.length; i++) {
+                        cards[i].id = 'card-for-scraping-' + i;
+                    }
+                ");
 
                 var content = await page.ContentAsync();
                 var context = BrowsingContext.New(Configuration.Default);
                 var document = await context.OpenAsync(req => req.Content(content));
 
-                var cards = document.QuerySelectorAll("td.model-short-info");
+                var cards = document.QuerySelectorAll("table.model-short-block");
 
                 foreach (var card in cards)
                 {
                     try
                     {
-                        var nameTag = card.QuerySelector("span.u")?.TextContent + " " + card.QuerySelector("span.list-conf-name.ib.nobr")?.TextContent;
-                        if (string.IsNullOrEmpty(nameTag)) continue;
+                        var modelInfo = card.QuerySelector("td.model-short-info");
+                        if (modelInfo == null) continue;
+                        
+                        var uSpan = modelInfo.QuerySelector("span.u")?.TextContent.Trim();
+                        if (string.IsNullOrEmpty(uSpan)) continue;
 
-                        var boxOem = nameTag.EndsWith("BOX") ? "box" : "oem";
-                        var name = nameTag.Substring(0, nameTag.Length - 4);
-                        var link = "https://ek.ua" + card.QuerySelector("a.model-short-title.no-u")?.GetAttribute("href");
+                        var imageTask = page.GetByAltText($"Процесор {uSpan}").ScreenshotAsync();
+                        
+                        var detailsDiv = modelInfo.QuerySelector("div.m-s-f2");
+                        (string socket, int tdp, string ram) = GetModelDetails(detailsDiv);
 
-                        var detailsDiv = card.QuerySelector("div.m-s-f2");
-                        if (detailsDiv == null) continue;
+                        var confList = modelInfo.QuerySelector("div.m-c-f1-pl--button");
+                        
+                        var image = await imageTask;
 
-                        var details = detailsDiv.ChildNodes;
-                        string tdp = "", socket = "", ram = "";
-
-                        foreach (var detail in details)
+                        if (confList != null)
                         {
-                            if (detail.TextContent.Contains("Socket"))
-                                socket = detail.ChildNodes[1].TextContent;
-                            else if (detail.TextContent.Contains("TDP"))
-                                tdp = detail.ChildNodes[1].TextContent.Replace("Вт", "").Trim();
-                            else if (detail.TextContent.Contains("ОЗП"))
-                                ram = detail.ChildNodes[1].TextContent;
+                            var confItems = confList.QuerySelectorAll("span.ib").ToList();
+                            
+                            for (var i = 0; i < confItems.Count; i++)
+                            {
+                                var item = confItems[i];
+                                if (item.ClassList.Contains("out-of-stock")) continue;
+                                
+                                if (!item.ClassList.Contains("current"))
+                                {
+                                    var cardId = card.Id;
+                                    var cardLocator = page.Locator($"#{cardId}");
+                                    var submodelLocator = cardLocator.Locator("div.m-c-f1-pl--button span.ib").Nth(i);
+                                    await submodelLocator.ClickAsync();
+                                    await page.WaitForLoadStateAsync(LoadState.NetworkIdle);
+                                }
+                                string submodelName = item.TextContent.Replace("\n", " ").Trim();
+                                submodelName = System.Text.RegularExpressions.Regex.Replace(submodelName, @"\s+", " ");
+                                var packing = submodelName.EndsWith("OEM") ? "oem" : "box";
+                                CreateAndAddCpu(cpus, uSpan, packing, socket, tdp, ram, image, card);
+                            }
                         }
-
-                        if (string.IsNullOrEmpty(socket) || string.IsNullOrEmpty(tdp) || string.IsNullOrEmpty(ram)) continue;
-
-                        var ramSplitted = ram.Split(", ");
-                        var ramMax = ram.StartsWith("макс") ? ram.Split(" ")[1].Replace("ГБ", "") : "";
-                        if (ramMax.Length > 0)
+                        else
                         {
-                            ramMax = ramMax.Substring(0, ramMax.Length - 2);
+                            var nameTag = modelInfo.QuerySelector("span.u")?.TextContent + " " + modelInfo.QuerySelector("span.list-conf-name.ib.nobr")?.TextContent;
+                            var packing = nameTag.EndsWith("OEM") ? "oem" : "box";
+                            CreateAndAddCpu(cpus, uSpan, packing, socket, tdp, ram, image, card);
                         }
-                        string ramGen = "", ramFreq = "";
-
-                        if (ramSplitted.Length > 1 || !ram.StartsWith("макс"))
-                        {
-                            var ramInfo = ramSplitted.Last().Replace("МГц", "").Trim().Split(" ");
-                            ramGen = ramInfo[0];
-                            ramFreq = ramInfo[1];
-                        }
-
-                        cpus.Add(new Cpu
-                        {
-                            Name = name,
-                            Link = link,
-                            Socket = socket,
-                            Tdp = tdp,
-                            RamMax = ramMax,
-                            RamGen = ramGen,
-                            RamFreq = ramFreq,
-                            BoxOem = boxOem
-                        });
                     }
                     catch (Exception e)
                     {
                         Console.WriteLine($"Skipping a malformed card. Error: {e.Message}");
-                        continue;
                     }
                 }
 
                 var nextButton = document.QuerySelector("a.ib.pager-next");
                 pageLink = nextButton != null ? "https://ek.ua" + nextButton.GetAttribute("href") : null;
+                if (pageLink == null) Console.WriteLine("End of catalog reached.");
             }
         }
         catch (Exception e)
@@ -128,11 +116,66 @@ public class CpuScraper
             await browser.CloseAsync();
         }
 
-        
         using (var writer = new StreamWriter(FilePath))
         using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
         {
             csv.WriteRecords(cpus);
         }
+        
+        Console.WriteLine($"Successfully saved {cpus.Count} CPUs to '{FilePath}'.");
+    }
+
+    private static (string, int, string) GetModelDetails(IElement? detailsDiv)
+    {
+        string socket = "", ram = "";
+        int tdp = 0;
+        if (detailsDiv == null) return (socket, tdp, ram);
+
+        foreach (var detail in detailsDiv.ChildNodes)
+        {
+            var text = detail.TextContent;
+            if (text.Contains("Socket"))
+                socket = detail.ChildNodes[1].TextContent;
+            else if (text.Contains("TDP"))
+                int.TryParse(detail.ChildNodes[1].TextContent.Replace("Вт", "").Trim(), out tdp);
+            else if (text.Contains("ОЗП"))
+                ram = detail.ChildNodes[1].TextContent;
+        }
+        return (socket, tdp, ram);
+    }
+
+    private static void CreateAndAddCpu(List<Cpu> list, string model, string? packing, string socket, int tdp, string ram, byte[] image, IElement card)
+    {
+        var priceInfo = card.QuerySelector("td.model-hot-prices-td");
+        var (priceRange, offers) = BaseScraper.GetPriceInfo(priceInfo);
+
+        var link = "https://ek.ua" + card.QuerySelector("div.model-short-links").QuerySelectorAll("a")
+            .Where(n => n.TextContent.Contains("Ціни")).FirstOrDefault().GetAttribute("link");
+
+        var ramSplitted = ram.Split(", ");
+        string ramGen = "";
+        int ramFreq = 0;
+
+        if (ramSplitted.Length > 1 || !ram.StartsWith("макс"))
+        {
+            var ramInfo = ramSplitted.Last().Replace("МГц", "").Trim().Split(" ");
+            ramGen = ramInfo[0];
+            int.TryParse(ramInfo[1], out ramFreq);
+        }
+        
+        list.Add(new Cpu
+        {
+            Id = Guid.NewGuid(),
+            Name = model,
+            Link = link,
+            Socket = socket,
+            Tdp = tdp,
+            RamGen = ramGen,
+            RamFrequency = ramFreq,
+            Packing = packing?.ToLower() == "box" ? CpuPacking.Box : CpuPacking.OEM,
+            Image = image,
+            PriceRange = priceRange,
+            Offers = offers
+        });
     }
 }
