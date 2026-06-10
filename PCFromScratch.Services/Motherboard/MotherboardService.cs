5630210d7145cf8921eb1ac952860c1c20c5ceb2
wwhasmile <uwuhasmile@proton.me>
2026-06-10 22:25:01 +0300

    Motherboard service formatting

--- PCFromScratch.Services/Motherboard/MotherboardService.cs
+++ PCFromScratch.Services/Motherboard/MotherboardService.cs
@@ -5,11 +5,9 @@ namespace PCFromScratch.Services;
 
 public class MotherboardService(IMotherboardRepository motherboardRepository) : IMotherboardService
 {
-    private readonly IMotherboardRepository _motherboardRepository = motherboardRepository;
-
     public async IAsyncEnumerable<MotherboardDtoModel> GetMotherboards(string? socket = null)
     {
-        await foreach (var motherboard in _motherboardRepository.GetMotherboards(socket))
+        await foreach (var motherboard in motherboardRepository.GetMotherboards(socket))
             yield return new(motherboard.Id, motherboard.Name, motherboard.Socket, motherboard.FormFactor,
                 motherboard.Chipset, motherboard.RamGeneration, motherboard.RamSlots, motherboard.RamFrequency,
                 motherboard.HasM2Slot, motherboard.ImageUrl, motherboard.MinPrice, motherboard.MaxPrice);
@@ -17,7 +15,7 @@ public class MotherboardService(IMotherboardRepository motherboardRepository) : 
 
     public async Task<MotherboardDtoModel?> GetMotherboard(Guid id)
     {
-        var motherboard = await _motherboardRepository.GetMotherboard(id);
+        var motherboard = await motherboardRepository.GetMotherboard(id);
         if (motherboard is null) return null;
 
         return new(motherboard.Id, motherboard.Name, motherboard.Socket, motherboard.FormFactor, motherboard.Chipset,
@@ -27,7 +25,7 @@ public class MotherboardService(IMotherboardRepository motherboardRepository) : 
 
     public async IAsyncEnumerable<OfferDtoModel> GetMotherboardOffers(Guid id)
     {
-        var motherboard = await _motherboardRepository.GetMotherboard(id);
+        var motherboard = await motherboardRepository.GetMotherboard(id);
         if (motherboard is not null)
             foreach (var offer in motherboard.Offers)
                 yield return new(offer.ShopName, offer.Price, offer.City);
