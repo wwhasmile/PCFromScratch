ce298b7cc6ac83f6f0623c91039b4a61dbfb5f32
wwhasmile <uwuhasmile@proton.me>
2026-06-10 21:23:28 +0300

    IRamService

---
+++ PCFromScratch.Services/Ram/IRamService.cs
@@ -0,0 +1,10 @@
+namespace PCFromScratch.Services;
+
+public interface IRamService
+{
+    IAsyncEnumerable<RamDtoModel> GetRams(string? generation = null);
+
+    Task<RamDtoModel?> GetRam(Guid id);
+
+    IAsyncEnumerable<RamDtoModel> GetRamOffers(Guid id);
+}
