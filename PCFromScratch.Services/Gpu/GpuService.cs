bca7fc3a57b2612e41e1acaedf205cbddb392c17
wwhasmile <uwuhasmile@proton.me>
2026-06-10 22:23:35 +0300

    Gpu service formatting

--- PCFromScratch.Services/Gpu/GpuService.cs
+++ PCFromScratch.Services/Gpu/GpuService.cs
@@ -7,17 +7,15 @@ namespace PCFromScratch.Services;
 
 public class GpuService(IGpuRepository gpuRepository) : IGpuService
 {
-    private readonly IGpuRepository _gpuRepository = gpuRepository;
-
     public async IAsyncEnumerable<GpuDtoModel> GetGpus()
     {
-        await foreach (var gpu in _gpuRepository.GetGpus())
+        await foreach (var gpu in gpuRepository.GetGpus())
             yield return new(gpu.Id, gpu.Name, gpu.Tdp, gpu.Length, gpu.ImageUrl, gpu.MinPrice, gpu.MaxPrice);
     }
 
     public async Task<GpuDtoModel?> GetGpu(Guid id)
     {
-        var gpu = await _gpuRepository.GetGpu(id);
+        var gpu = await gpuRepository.GetGpu(id);
         if (gpu is null) return null;
 
         return new(gpu.Id, gpu.Name, gpu.Tdp, gpu.Length, gpu.ImageUrl, gpu.MinPrice, gpu.MaxPrice);
@@ -25,11 +23,9 @@ public class GpuService(IGpuRepository gpuRepository) : IGpuService
 
     public async IAsyncEnumerable<OfferDtoModel> GetGpuOffers(Guid id)
     {
-        var gpu = await _gpuRepository.GetGpu(id);
+        var gpu = await gpuRepository.GetGpu(id);
         if (gpu is not null)
-        {
             foreach (var offer in gpu.Offers)
                 yield return new(offer.ShopName, offer.Price, offer.City);
-        }
     }
 }
