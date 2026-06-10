82110c86c2ed8fffc2191f078e665233b0c5fa38
wwhasmile <uwuhasmile@proton.me>
2026-06-10 12:32:53 +0300

    Internal drive service

---
+++ PCFromScratch.Services/InternalDrive/InternalDrviceService.cs
@@ -0,0 +1,35 @@
+using PCFromScratch.DTOModels;
+using PCFromScratch.Repository;
+
+namespace PCFromScratch.Services;
+
+public class InternalDriveService(IInternalDriveRepository internalDriveRepository) : IInternalDriveService
+{
+    private readonly IInternalDriveRepository _internalDriveRepository = internalDriveRepository;
+
+    public async IAsyncEnumerable<InternalDriveDtoModel> GetInternalDrives(string? type = null, int? capacity = null)
+    {
+        await foreach (var internalDrive in _internalDriveRepository.GetInternalDrives(type, capacity))
+            yield return new(internalDrive.Id, internalDrive.Name, internalDrive.Capacity, internalDrive.Type,
+                internalDrive.Port, internalDrive.ImageUrl, internalDrive.MinPrice, internalDrive.MaxPrice);
+    }
+
+    public async Task<InternalDriveDtoModel?> GetInternalDrive(Guid id)
+    {
+        var internalDrive = await _internalDriveRepository.GetInternalDrive(id);
+        if (internalDrive is null) return null;
+
+        return new(internalDrive.Id, internalDrive.Name, internalDrive.Capacity, internalDrive.Type,
+            internalDrive.Port, internalDrive.ImageUrl, internalDrive.MinPrice, internalDrive.MaxPrice);
+    }
+
+    public async IAsyncEnumerable<OfferDtoModel> GetInternalDriveOffers(Guid id)
+    {
+        var internalDrive = await _internalDriveRepository.GetInternalDrive(id);
+        if (internalDrive is not null)
+        {
+            foreach (var offer in internalDrive.Offers)
+                yield return new(offer.ShopName, offer.Price, offer.City);
+        }
+    }
+}
