06e8923e46633ab1af01f6279f55e50cae424069
wwhasmile <uwuhasmile@proton.me>
2026-06-11 14:56:00 +0300

    GPU benchmark service fixing method name

--- PCFromScratch.Services/GpuBenchmark/IGpuBenchmarkService.cs
+++ PCFromScratch.Services/GpuBenchmark/IGpuBenchmarkService.cs
@@ -5,5 +5,5 @@ namespace PCFromScratch.Services;
 public interface IGpuBenchmarkService
 {
     Task<GpuBenchmarkDtoModel?> GetGpuBenchmark(Guid id);
-    Task<GpuBenchmarkDtoModel?> GeGpuBenchmark(string name);
+    Task<GpuBenchmarkDtoModel?> GetGpuBenchmark(string name);
 }
